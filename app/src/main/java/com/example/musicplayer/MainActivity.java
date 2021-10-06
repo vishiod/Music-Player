a3e1e98fbd30021e03ca30395effd5f401f88575
vishiod <vishalsharma01386@gmail.com>
2021-10-07 00:31:15 +0530

    Second Commit

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.musicplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Environment;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -22,16 +26,45 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     ArrayList<String> songs;
 
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return songs.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            @SuppressLint({"ViewHolder", "InflateParams"}) View myView = getLayoutInflater().inflate(R.layout.list_item, null);
+            TextView textSong = myView.findViewById(R.id.txtSongName);
+
+            textSong.setSelected(true);
+            textSong.setText(songs.get(i));
+
+            return myView;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listViewSong);
+        listView = findViewById(R.id.listViewSongs);
         runTimePermission();
     }
 
-    public void runTimePermission(){
+    public void runTimePermission() {
         Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new PermissionListener() {
             @Override
             public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
@@ -50,29 +83,32 @@ public class MainActivity extends AppCompatActivity {
         }).check();
     }
 
-    public ArrayList<File> findSong(File file){
+    public void displaySongs() {
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory()); //Takes Songs from only Mobile not from SD Card
+        songs = new ArrayList<>();
+
+        for (File song : mySongs) {
+            songs.add(song.getName().replace(".mp3", "").replace(".wav", ""));
+        }
+
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+    }
+
+    public ArrayList<File> findSong(File file) {
         ArrayList<File> arrayList = new ArrayList<>();
         File[] files = file.listFiles();
 
-        for (File singleFile: files){
-            if(singleFile.isDirectory() && !singleFile.isHidden())
+        assert files != null;
+
+        for (File singleFile : files) {
+            if (singleFile.isDirectory() && !singleFile.isHidden())
                 arrayList.addAll(findSong(singleFile));
-            else
-                if(singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav"))
+
+            else if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav"))
                     arrayList.add(singleFile);
         }
         return arrayList;
     }
 
-    public void displaySongs(){
-        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
-        songs = new ArrayList<>();
-
-        for(File song: mySongs){
-            songs.add(song.getName().toString().replace(".mp3","").replace(".wav",""));
-        }
-
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, songs);
-        listView.setAdapter(myAdapter);
-    }
 }
